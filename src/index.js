76f0f4d5384415b7ca0b0103617487e6798a1022
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-09-28 11:54:27 -0400

    added cards

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './assets/index.css';
+import App from './components/App';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+
+// React.render(<App />, document.body);
