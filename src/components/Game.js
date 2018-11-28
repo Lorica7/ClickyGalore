0a8d50caeddf530341cb6a4b22ff0da9e24159db
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-11-28 12:47:48 -0500

    changed setState to one call

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,22 +16,22 @@ class Game extends Component {
     this.setState = this.setState.bind(this);
   }
 
-
-
   changeClicked = id => {
     let hero = this.state.heroes[id - 1];
-    // let heroes = this.state.heroes;
     console.log(hero)
     if (hero.clicked === false) {
       this.scoreUp()
       this.handleIncrement(id)
         .then((heroesCopy) => {
-          this.setState({ heroes: heroesCopy })
-          console.log(this.state.heroes)
+          this.shuffleCards(heroesCopy)
+          console.log(heroesCopy)
         })
-        .then(() => {
-          console.log(this.state.heroes)
-          this.shuffleCards(this.state.heroes)
+        .then((heroesCopy) => {
+          this.setState({
+            heroes: heroesCopy,
+            score: this.state.score + 1
+          })
+          console.log(this.state)
         })
     } else if (hero.clicked === true) {
       this.handleReset()
@@ -40,73 +40,69 @@ class Game extends Component {
 
 
 
-  shuffleCards = (heroes) => {
+shuffleCards = (heroesCopy) => {
+  return new Promise(function (resolve, reject) {
     var j, x, i;
-    for (i = heroes.length - 1; i > 0; i--) {
+    for (i = heroesCopy.length - 1; i > 0; i--) {
       j = Math.floor(Math.random() * (i + 1));
       x = heroes[i];
       heroes[i] = heroes[j];
       heroes[j] = x;
-      this.setState({ heroes: heroes })
-    };
+    } resolve(console.log("shuffled", heroesCopy))
+  })
+}
 
-  }
+handleIncrement = (id) => {
+  let heroes = this.state.heroes;
+  let heroesCopy = JSON.parse(JSON.stringify(heroes))
+  return new Promise(function (resolve, reject) {
+    for (let i = 0; i < heroes.length; i++) {
+      if (id === heroes[i].id) {
+        console.log("Matching ID!")
+        heroesCopy[id - 1].clicked = true
+      } else console.log("no match")
+    }
+    resolve(heroesCopy)
+  })
+}
 
-  scoreUp = () => {
-    this.setState({ score: this.state.score + 1 });
-  }
+handleReset = () => {
+  let heroes = this.state.heroes;
+  this.setState({ score: 0, heroes: { clicked: false } });
 
-  handleIncrement = (id) => {
-    let heroes = this.state.heroes;
-    let heroesCopy = JSON.parse(JSON.stringify(heroes))
-    return new Promise(function (resolve, reject) {
-      for (let i = 0; i < heroes.length; i++) {
-        if (id === heroes[i].id) {
-          console.log("Matching ID!")
-          heroesCopy[id - 1].clicked = true
-        } else console.log("no match")
-      }
-      resolve(heroesCopy)
-    })
-  }
-
-  handleReset = () => {
-    let heroes = this.state.heroes;
-    this.setState({ score: 0, heroes: { clicked: false } });
-
-    alert("You've already clicked that one. Try Again")
-    this.shuffleCards(heroes)
-  }
+  alert("You've already clicked that one. Try Again")
+  this.shuffleCards(heroes)
+}
 
 
-  render() {
+render() {
 
-    return (
+  return (
 
-      <div className="container">
-        <div className="containerStyle">
-              {this.state.heroes.map(hero => (
-                <Card
-                  key={hero.id}
-                  image={hero.image}
-                  id={hero.id}
-                  clicked={hero.clicked}
-                  changeClicked={this.changeClicked.bind(this)}
-                />
-              )
-              )}
+    <div className="container">
+      <div className="containerStyle">
+        {this.state.heroes.map(hero => (
+          <Card
+            key={hero.id}
+            image={hero.image}
+            id={hero.id}
+            clicked={hero.clicked}
+            changeClicked={this.changeClicked.bind(this)}
+          />
+        )
+        )}
+      </div>
+      <div className="card text-center">
+        <div className="card-header bg-primary text-white">
+          Click Counter!
         </div>
-        <div className="card text-center">
-          <div className="card-header bg-primary text-white">
-            Click Counter!
-        </div>
-          <div className="card-body">
-            <p className="card-text">Click Count: {this.state.score}</p>
-          </div>
+        <div className="card-body">
+          <p className="card-text">Click Count: {this.state.score}</p>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 };
 
 
